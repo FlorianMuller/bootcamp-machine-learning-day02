174e59dab93dcfd62c5ebb6d284b821302141147
Florian Muller <fmuller@student.42.fr>
2020-06-09 21:32:43 +0200

    ex03 done

--- ex03/prediction.py
+++ ex03/prediction.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-def simple_predict(x, theta):
+def predict_(x, theta):
     """
     Computes the prediction vector y_hat from two non-empty numpy.ndarray.
     Args:
-        x: has to be an numpy.ndarray, a matrix of dimension m * n.
+        x: has to be an numpy.ndarray, a vector of dimension m * n.
         theta: has to be an numpy.ndarray, a vector of dimension (n + 1) * 1.
     Returns:
         y_hat as a numpy.ndarray, a vector of dimension m * 1.
@@ -34,26 +34,26 @@ if __name__ == "__main__":
 
     # Example 1:
     theta1 = np.array([5, 0, 0, 0])
-    print(simple_predict(x, theta1))
+    print(predict_(x, theta1))
     # Ouput:
     # array([5., 5., 5., 5.])
     # Do you understand why y_hat contains only 5's here?
 
     # Example 2:
     theta2 = np.array([0, 1, 0, 0])
-    print(simple_predict(x, theta2))
+    print(predict_(x, theta2))
     # Output:
     # array([1., 4., 7., 10.])
     # Do you understand why y_hat == x[:,0] here?
 
     # Example 3:
     theta3 = np.array([-1.5, 0.6, 2.3, 1.98])
-    print(simple_predict(x, theta3))
+    print(predict_(x, theta3))
     # Output:
     # array([9.64, 24.28, 38.92, 53.56])
 
     # Example 4:
     theta4 = np.array([-3, 1, 2, 3.5])
-    print(simple_predict(x, theta4))
+    print(predict_(x, theta4))
     # Output:
     # array([12.5, 32., 51.5, 71.])
